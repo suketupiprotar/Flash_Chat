d670ed9ef4a6d8d6480577b63a618a508dc41917
suketupiprotar <suektupiprotar9930@gmail.com>
2022-10-07 22:52:36 +0530

    flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
